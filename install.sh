cc9b2200112ce58158bb87fa31d12d53e51521f7
oshida <five52113@gmail.com>
2019-03-16 19:02:36 +0900

    edit: mvコマンド追加 php不在

--- install.sh
+++ install.sh
@@ -9,7 +9,10 @@ amazon-linux-extras install php7.2 -y
 systemctl start php-fpm.service
 systemctl enable php-fpm.service
 
+mv phpinfo.php /usr/share/nginx/html
+
 cp -p /etc/php-fpm.d/www.conf /etc/php-fpm.d/www.conf.org
 sed -i -e 's/user = apache/user = nginx/g' /etc/php-fpm.d/www.conf
 sed -i -e 's/group = apache/user = nginx/g' /etc/php-fpm.d/www.conf
+
 
